fdcda74386f8c04c9dbe6018daa0264e908576d8
Vinson Chuong <Vinson.Chuong@gmail.com>
2010-12-30 16:42:09 -0800

    Bug-Fix: rails-settings is a plugin and does not need to be included as a gem in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -15,7 +15,7 @@ Rails::Initializer.run do |config|
   # config.autoload_paths += %W( #{RAILS_ROOT}/extras )
 
   # Specify gems that this application depends on and have them installed with rake gems:install
-  config.gem 'rails-settings', :lib => 'settings'
+  config.gem 'facets'
 
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
