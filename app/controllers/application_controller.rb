7513635fc51d24da936d469c8e8e4ecef5549a52
Jacky Wu <jackywu07@berkeley.edu>
2011-01-02 18:04:59 -0800

    refactored, and merged cas_example code into main application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,14 +5,11 @@ require 'casclient/frameworks/rails/filter'
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   helper_method :current_user # make current_user a helper method, which can be called from views
-  
+  before_filter CASClient::Frameworks::Rails::Filter  # Ensures logging in with CAS before seeing ANY view at all
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
-
-  # Ensures logging in with CAS before seeing ANY view at all
-  before_filter CASClient::Frameworks::Rails::Filter
 
   # Defines the current_user method for use by the CanCan gem
   def current_user
@@ -24,16 +21,6 @@ class ApplicationController < ActionController::Base
   end
   
   def logout
-    #st = session[:cas_last_valid_ticket]
-    #delete_service_session_lookup(st) if st
-    #reset_session
-    #protocol = request.ssl? ? 'https://' : 'http://'
-    #app_url = "localhost:3000/"
-    #logout_url = "https://auth-test.berkeley.edu/cas/logout"
-    
-    #redirect_to("#{logout_url}?url=#{protocol}#{app_url}")    
-    
-    # CASClient::Frameworks::Rails::Filter.logout(self) will call the above lines. What's the reason for writing them again?
     CASClient::Frameworks::Rails::Filter.logout(self)
   end
   
