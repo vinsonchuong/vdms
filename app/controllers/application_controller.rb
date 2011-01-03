3859d9d4cf265cfa801f873785573f53deecccfe
Jacky Wu <jackywu07@berkeley.edu>
2011-01-02 18:35:38 -0800

    refactored, and merged cas_example code into main application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,23 @@ class ApplicationController < ActionController::Base
     session[:cas_user]
   end
   
+  
+  # This index function has be take off from cas_example_controller. Nothing has been modified.
+  def index
+    ldap = Net::LDAP.new
+    ldap.host = "ldap-test.berkeley.edu"
+    filter = Net::LDAP::Filter.eq( "uid", session[:cas_user])
+    attrs = []
+    
+    @ldapparams = Hash.new
+    ldap.search( :base => "ou=people,dc=berkeley,dc=edu", :filter => filter, :return_result => true ) do |entry|
+      
+      entry.attribute_names.each do |n|
+        @ldapparams[n] = entry[n]
+      end
+    end
+  end
+  
   def logout
     CASClient::Frameworks::Rails::Filter.logout(self)
   end
