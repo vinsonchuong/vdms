83e0b2d9be16686097a2cbe9eff1cf7b2ad0bf0f
Vinson Chuong <Vinson.Chuong@gmail.com>
2011-01-27 18:12:29 -0800

    Pull CSV code into Importable mixin.

---
+++ app/models/importable.rb
@@ -0,0 +1,35 @@
+module Importable
+  module ClassMethods
+    def import_csv(csv_text)
+      result = []
+      transaction do
+        FasterCSV.parse(csv_text, :headers => :first_row) do |row|
+          new_person = self.new(self.convert_attributes(row))
+          result << new_person
+          new_person.save
+        end
+        raise ActiveRecord::Rollback unless result.all? {|r| r.valid?} 
+      end
+      result
+    end
+  
+    def convert_attributes(csv_row)
+      attributes = csv_row.to_hash
+      attributes.update_keys {|col| self::ATTRIBUTES[col]}
+      attributes.delete_if {|accessor, value| accessor.nil?}
+      attributes.update_each do |accessor, value|
+        conversion = case self::ATTRIBUTE_TYPES[accessor]
+        when :integer then :to_i
+        when :boolean then :to_b
+        else :to_s
+        end
+        {accessor, value.send(conversion)}
+      end
+      attributes
+    end
+  end
+
+  def self.included(base)
+    base.extend ClassMethods
+  end
+end
