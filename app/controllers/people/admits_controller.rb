d4412036eca23623ade6151acacfee1089b8629c
Jacky Wu <jackywu07@berkeley.edu>
2011-02-01 23:25:48 -0800

    admit_ranking implemented, still tracking down few bugs, still need to write spec

--- app/controllers/people/admits_controller.rb
+++ app/controllers/people/admits_controller.rb
@@ -6,7 +6,7 @@ class AdmitsController < PeopleController
     faculty = Faculty.find(params[:faculty_id])
     params.each do |area_key, area_value|
       @admit = Admit.find(:all, :conditions => ["area1 = ? OR area2 = ?", area_value, area_value])
-      @admits << @admit if faculty.admit_rankings.find(:all, :conditions => ["admit_id != ?", @admit.id])
+      @admits << @admit #if faculty.admit_rankings.find(:all, :conditions => ["admit_id != ?", @admit.id])
     end
     @admits.uniq!
   end
