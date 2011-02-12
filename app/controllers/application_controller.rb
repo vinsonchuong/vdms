893e341b9d8cad4e23eba73508058b9b9324d444
Jacky Wu <jackywu07@berkeley.edu>
2011-02-12 05:53:51 -0800

    testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,8 @@ require 'casclient/frameworks/rails/filter'
 class ApplicationController < ActionController::Base
   helper :all
   prepend_before_filter CASClient::Frameworks::Rails::Filter
-  #before_filter :get_current_user
-  #before_filter :verify_current_user
+  before_filter :get_current_user
+  before_filter :verify_current_user
   
   
   self.allow_forgery_protection = false
@@ -13,12 +13,9 @@ class ApplicationController < ActionController::Base
     ldap_entry = LDAPWrapper.find_by_ldap_id(session[:cas_user])
     render :controller => "root", :action => "access_denied" if !ldap_entry
     
-    @current_user = ldap_entry.model.find_by_ldap_id(session[:cas_user])
-    #@current_user = ldap_entry.model.find(:first)
-    #puts @current_user.first_name
+    @current_user = Person.find_by_ldap_id(session[:cas_user])
     ldap_entry.model.new(ldap_entry.attributes).save(false) if (!@current_user and ldap_entry)
-    @current_user = ldap_entry.model.find_by_ldap_id(session[:cas_user])
-    #@current_user = Person.find(:first)
+    @current_user = Person.find_by_ldap_id(session[:cas_user])
   end
   
   def verify_current_user
