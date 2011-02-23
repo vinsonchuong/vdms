f27a9d1b323f590cea0aa68bc488f44cd012235a
Vinson Chuong <Vinson.Chuong@gmail.com>
2011-02-23 09:41:45 -0800

    Unknown requirement on Settings#meeting_times attribute. Will investigate later.

--- db/migrate/20110222031443_remove_meeting_times_from_settings.rb
+++
@@ -1,9 +0,0 @@
-class RemoveMeetingTimesFromSettings < ActiveRecord::Migration
-  def self.up
-    remove_column :settings, :meeting_times
-  end
-
-  def self.down
-    add_column :settings, :meeting_times, :string
-  end
-end
